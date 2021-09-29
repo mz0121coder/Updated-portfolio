aa974454bd87c8c1a87fba8e874fd5accfdfa89f
Mohammad Zafar <mzcoder7@gmail.com>
2021-09-29 20:26:27 +0100

    Declare variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [80, 75, 70, 65, 60, 55, 55];
