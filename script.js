a79421b08d3a6e844a9e8bbfec8287b4785a15e7
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-20 23:34:16 +0100

    New projects/skills

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [80, 75, 70, 65, 60, 55, 55];
+const progressBarPercents = [75, 70, 70, 55, 55, 55, 55];
 
 window.addEventListener("scroll", () => {
   mainFn();
